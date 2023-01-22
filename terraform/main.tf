6a19877dce92725da13db9f6d8366a0123ec567a
Maksim <meison98@bk.ru>
2023-01-22 12:41:46 +0300

    ci - added terraform aws_instance configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "web-app" {
+  count = 1
   ami = "ami-00874d747dde814fa"
   instance_type = "t3.micro"
+
+  tags = {
+    Name = "Web App Server"
+    Owner = "mmozhorov"
+    Project = "TSIN-PORTAL-BE-V2"
+  }
 }
