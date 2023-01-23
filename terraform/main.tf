9e1174471d8f89474a226765f0f95b28af9097b1
Maksim <meison98@bk.ru>
2023-01-23 11:43:32 +0300

    ci - added terraform elastic IP for Web App

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_eip" "web-app-static-ip" {
+  instance = aws_instance.web-app.id
+}
+
 resource "aws_instance" "web-app" {
   count = 1
   ami = "ami-00874d747dde814fa"
@@ -13,5 +17,9 @@ resource "aws_instance" "web-app" {
     Name = "Web App Server"
     Owner = "mmozhorov"
     Project = "TSIN-PORTAL-BE-V2"
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
