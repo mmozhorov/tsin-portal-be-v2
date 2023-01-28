d2a5df272a78261237c78bbd9f22bd04505a40a4
Maksim <meison98@bk.ru>
2023-01-28 12:20:03 +0300

    ci - added Dockerfile

--- web-app/src/main.ts
+++ web-app/src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   SwaggerModule.setup('api-docs', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 80);
 }
 bootstrap();
